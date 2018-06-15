19cd26a5e71b080352f5e763ab13641ec7953675
guanlin.fei <1612184086@qq.com>
2018-06-15 13:54:42 +0800

    change Ultrasonic_Car() weight

--- monkey_bot.ts
+++ monkey_bot.ts
@@ -121,7 +121,7 @@ namespace monkeybot_输入类 {
 	
 	//% blockId=monkeybot_ultrasonic_car block="ultrasonic return distance(cm)"
     //% color="#0fbc11"
-    //% weight=98
+    //% weight=100
     //% blockGap=10
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
     export function Ultrasonic_Car(): number {
