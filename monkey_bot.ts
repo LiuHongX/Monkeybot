862d8df0facc66e1bcc49cc84d95649a7e93aa8d
guanlin.fei <1612184086@qq.com>
2018-07-05 14:40:48 +0800

    14:40

--- monkey_bot.ts
+++ monkey_bot.ts
@@ -10,7 +10,15 @@ namespace monkeybot_小车类 {
 	//% blockId=monkeybot_car_reset block="小车复位"
     //% weight=100
     export function CarReset(): void {
-        
+        let rgbLight: monkeybot_RGB灯.Strip = null
+		basic.showIcon(IconNames.Heart)
+		monkeybot_显示类.MatrixClear()
+		monkeybot_电机类.RGB_Car_Big2(monkeybot_电机类.enColor.OFF)
+		monkeybot_电机类.Servo_Car(monkeybot_电机类.enServo.S1, 85)
+		monkeybot_电机类.MotorStopAll()
+		MonkeybotIR.init(Pins.P8)
+		rgbLight= monkeybot_RGB灯.create(DigitalPin.P16, 12, monkeybot_RGB灯.NeoPixelMode.RGB)
+		rgbLight.clear()
     }
 }
 
