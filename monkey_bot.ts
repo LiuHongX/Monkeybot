c29df56f876200f7ad11b65d86078334f2a4b797
guanlin.fei <1612184086@qq.com>
2018-08-13 11:09:11 +0800

    11:09

--- monkey_bot.ts
+++ monkey_bot.ts
@@ -5,31 +5,6 @@ load dependency
 "mbit": "file:../pxt-mbit"
 */
 
-
-
-//% color=50 weight=19
-//% icon="\uf1eb"
-namespace MonkeybotIR {
-
-    /**
-     * button pushed.
-     */
-    //% blockId=ir_received_left_event
-    //% block="当|%btn|按钮按下"
-    export function onPressEvent(btn: RemoteButton, body: () => void): void{
-		MonkeybotIR_C.onPressEvent(btn,body);
-	}
-
-    /**
-     * initialises local variablesssss
-     */
-    //% blockId=ir_init
-    //% block="连接遥控接收到引脚%pin"
-    export function init(pin: Pins): void{
-		MonkeybotIR_C.init(pin);
-	}
-}
-
 //% color="#87CEEB" weight=4 icon="\uf1b6"
 namespace monkeybot_小车类 {
 	export enum rgbColor {
---
+++ shims.d.ts
@@ -0,0 +1,24 @@
+// Auto-generated. Do not edit.
+
+
+
+    //% color=50 weight=19
+    //% icon="\uf1eb"
+declare namespace MonkeybotIR {
+
+    /**
+     * button pushed.
+     */
+    //% blockId=ir_received_left_event
+    //% block="当|%btn|按钮按下" shim=MonkeybotIR::onPressEvent
+    function onPressEvent(btn: RemoteButton, body: () => void): void;
+
+    /**
+     * initialises local variablesssss
+     */
+    //% blockId=ir_init
+    //% block="连接遥控接收到引脚%pin" shim=MonkeybotIR::init
+    function init(pin: Pins): void;
+}
+
+// Auto-generated. Do not edit. Really.
