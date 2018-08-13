440f55bbded265eda14f6da1a4895c3b81d5842c
guanlin.fei <1612184086@qq.com>
2018-08-13 10:34:22 +0800

    10：34

--- shims.d.ts
+++ shims.d.ts
@@ -11,14 +11,14 @@ declare namespace MonkeybotIR {
      */
     //% blockId=ir_received_left_event
     //% block="当|%btn|按钮按下" shim=MonkeybotIR::onPressEvent
-    function onPressEvent(btn: RemoteButton, body: () => void): void;
+    export function onPressEvent(btn: RemoteButton, body: () => void): void;
 
     /**
      * initialises local variablesssss
      */
     //% blockId=ir_init
     //% block="连接遥控接收到引脚%pin" shim=MonkeybotIR::init
-    function init(pin: Pins): void;
+    export function init(pin: Pins): void;
 }
 
 // Auto-generated. Do not edit. Really.
